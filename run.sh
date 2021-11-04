d54b9c66a4832fe40b3b270c1cb1a19a01691e0f
VictorTao1998 <jit124@ucsd.edu>
2021-11-03 20:52:15 -0700

    init

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PYTHONWARNINGS="ignore"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u /jianyu-fast-vol/reprojection/train_psmnet_sim_ir_reprojection.py \
+python -m torch.distributed.launch /jianyu-fast-vol/reprojection/train_psmnet_sim_ir_reprojection.py \
 --logdir='/jianyu-fast-vol/eval/reprojection_p1' \
 --config-file '/jianyu-fast-vol/reprojection/configs/remote_train_primitive_steps.yaml' \
 --loss-ratio 0 \
