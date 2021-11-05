009e2a31cbb9601d4b00983f1f776ed50c74426e
VictorTao1998 <jit124@ucsd.edu>
2021-11-05 16:24:36 -0700

    test

--- run-test.sh
+++ run-test.sh
@@ -2,7 +2,6 @@
 export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /jianyu-fast-vol/reprojection/test_psmnet_reprojection.py \
---logdir='/jianyu-fast-vol/eval/reprojection_real_p2_ps40' \
 --config-file '/jianyu-fast-vol/reprojection/configs/remote_test.yaml' \
 --model '/jianyu-fast-vol/eval/reprojection_real_p1/models/model_48000.pth'
 --output '/jianyu-fast-vol/eval/reprojection_test_real_p1' \
