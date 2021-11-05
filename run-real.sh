e7d7a03f357507f784b1740367bd78df38548cdd
VictorTao1998 <jit124@ucsd.edu>
2021-11-05 02:27:26 -0700

    loss

--- run-real.sh
+++ run-real.sh
@@ -2,8 +2,8 @@
 export PYTHONWARNINGS="ignore"
 
 python -m torch.distributed.launch /jianyu-fast-vol/reprojection/train_psmnet_real_ir_reprojection.py \
---logdir='/jianyu-fast-vol/eval/reprojection_real_p2_ps35' \
+--logdir='/jianyu-fast-vol/eval/reprojection_real_p2_ps40' \
 --config-file '/jianyu-fast-vol/reprojection/configs/remote_train_primitive_steps.yaml' \
 --loss-ratio 0 \
 --pattern2 \
---patch_size 35
+--patch_size 40
